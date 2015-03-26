be5473f68a4dc724ac0dfd9ecb551bdcec60ef03
Christian Mayer <christian@fox21.at>
2015-03-26 10:44:07 +0100

    Version 1.1.0.

--- src/config.h
+++ src/config.h
@@ -4,7 +4,7 @@
 
 #define PROJECT_NAME "Keylogger"
 #define PROJECT_VERSION_MAJOR 1
-#define PROJECT_VERSION_MINOR 0
+#define PROJECT_VERSION_MINOR 1
 #define PROJECT_VERSION_PATCH 0
 #define PROJECT_COPYRIGHT "Copyright (C) 2009 Christian Mayer <http://fox21.at>"
 #define FILEEXT ".log"
